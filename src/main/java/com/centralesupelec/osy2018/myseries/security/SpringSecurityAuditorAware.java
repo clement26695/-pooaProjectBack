d17687868f56a8712aa5ec76ae9e20f3c0283fab
Clement Dessoude <clement.dessoude@gmail.com>
2018-10-20 15:50:41 +0200

    feat(security): add entities to manage security

---
+++ src/main/java/com/centralesupelec/osy2018/myseries/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.centralesupelec.osy2018.myseries.security;
+
+import java.util.Optional;
+
+import com.centralesupelec.osy2018.myseries.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
---
+++ src/main/java/com/centralesupelec/osy2018/myseries/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.centralesupelec.osy2018.myseries.security;
