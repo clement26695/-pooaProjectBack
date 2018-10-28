8e730e94e4813499813c809a87a9b29d2b9bbe1e
Clement Dessoude <clement.dessoude@gmail.com>
2018-10-28 15:09:10 +0100

    refactor(notifications): simplify cron usage with @Scheduled annotation

--- src/main/java/com/centralesupelec/osy2018/myseries/MyseriesApplication.java
+++ src/main/java/com/centralesupelec/osy2018/myseries/MyseriesApplication.java
@@ -1,26 +1,15 @@
 package com.centralesupelec.osy2018.myseries;
 
-import com.centralesupelec.osy2018.myseries.utils.cronjobs.CronThread;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MyseriesApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(MyseriesApplication.class, args);
 
-        CronThread t = new CronThread();
-        t.start();
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            t.arreter();
-        }
 	}
 }
